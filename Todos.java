4de972fe1e910c1aba7424ba0850b51c88de1c70
Daria Lacko <silaievadaria@gmail.com>
2020-12-26 13:10:07 +0100

    refactered class to do

--- Todos.java
+++ Todos.java
@@ -1,6 +1,31 @@
 class Todos {
-	public static void main(String[] args) {
-		String what = args != null && args.length > 0 && args[0] != null ?  args[0] : "World";
-		System.out.println("Hello "+ what);
-	}
+
+    private static void printMessage(String message) {
+        System.out.println(message);
+    }
+
+    private static String getWhat(String[] args) {
+        if (args != null && args.length > 0) {
+            return Todos.reardArguments(args);
+        } else {
+            return "Hello World!";
+        }
+    }
+
+    private static String reardArguments(String[] args) {
+        String result = "Priviet ";
+        for (int i = 0; i < args.length; i++) {
+            if (i == 0) {
+                result = result + args[i];
+            } else {
+                result = result + " and " + args[i];
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+        Todos.printMessage(Todos.getWhat(args));
+    }
 }
